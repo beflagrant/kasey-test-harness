4f3874bd94dcf57b0efae90f8933580b03ddc619
Ben Vandgrift <ben@vandgrift.com>
2021-01-20 14:09:33 -0500

    tests for "close" button, fixes access in specs

--- spec/views/kasey/kases_spec.rb
+++ spec/views/kasey/kases_spec.rb
@@ -1,27 +1,62 @@
 require 'rails_helper'
 
-describe 'sstuff', type: :request do
+describe 'Kasey::KasesController', type: :request do
     let(:user) { User.create(email: 'admin@example.com') }
-    context 'open kases' do
-        scenario 'do not display a close button' do
-            intake = Intake.create(email: 'test+open@example.com')
 
-            visit '/kasey/kases'
+    before do
+        allow_any_instance_of(Kasey::KasesController).to receive(:has_access?).and_return(true)
+    end
 
-            expect(page).not_to have_content 'Close'
+    context 'index' do
+        context 'open kases' do
+            scenario 'do not display a close button' do
+                intake = Intake.create(email: 'test+open@example.com')
+
+                visit '/kasey/kases'
+
+                expect(page).not_to have_content 'Close'
+            end
+        end
+
+        context 'previously viewed kases' do
+            scenario 'displays a close button' do
+                intake = Intake.create(email: 'test+reviewed@example.com')
+                intake.kase.review!
+
+                sign_in(user)
+
+                visit '/kasey/kases'
+
+                expect(page).to have_content 'Close'
+            end
         end
     end
 
-    context 'previously viewed kases' do
-        scenario 'display a close button' do
-            intake = Intake.create(email: 'test+reviewed@example.com')
+    context 'closed' do
+        let(:email) { 'test+closed@example.com' }
+
+        scenario 'displays closed kases' do
+            intake = Intake.create(email: email)
+            intake.kase.review!
+            intake.kase.close!
+
+            sign_in(user)
+
+            visit '/kasey/kases/closed'
+
+            expect(page).to have_content email
+        end
+
+        scenario 'elides kases not closed' do
+            intake = Intake.create(email: email)
             intake.kase.review!
 
             sign_in(user)
 
-            visit '/kasey/kases'
+            visit '/kasey/kases/closed'
 
-            expect(page).to have_content 'Close'
+            expect(page).not_to have_content email
         end
+
     end
 end

fe489548d93c03eedaaaf384ae4a43746ca6eaff
Ben Vandgrift <ben@vandgrift.com>
2021-06-16 17:12:40 -0400

    dependency updates

--- config/initializers/kasey.rb
+++ config/initializers/kasey.rb
@@ -1,6 +1,6 @@
 # kasey initializer
 #
-# to configure Kasey for use with your application, you need to 
+# to configure Kasey for use with your application, you need to
 # tell it a few things. `auth_required` must be a boolean value.
 # the rest of the configuration parameters can be a :symbol that is
 # the name of a function to call, or a -> { lambda }. in particular,
@@ -14,7 +14,7 @@
 #
 # the following example assumes Devise, and has a custom function for
 # authorization:
-# 
+#
 #     Kasey.configure do |c|
 #       c.auth_required = true
 #       c.authenticate_function = :authenticate_user! # from Devise
@@ -28,5 +28,6 @@ Kasey.configure do |c|
   c.authenticated_user_function = :current_user
   c.authorize_function = ->(user, kase) { true }
   c.email_domain = 'kasey-test-harness.com'
+  c.notify_email = 'test@kasey-test-harness.com'
   c.routing_pattern = /kasey/i
 end
